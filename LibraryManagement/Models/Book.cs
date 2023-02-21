19c32aea77e4457f9e1c28b8c7294eb8223c1ddc
alinoruzi <alinoruzi.ir@gmail.com>
2023-02-21 19:50:46 +0330

    .

--- LibraryManagement/Models/Book.cs
+++ LibraryManagement/Models/Book.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace LibraryManagement.Models
@@ -9,10 +10,13 @@ namespace LibraryManagement.Models
         public int Id { get; set; }
         [Required]
         [MaxLength(40)]
+        [DisplayName("عنوان")]
         public string Title { get; set; }
         [MaxLength(350)]
+        [DisplayName("توضیحات")]
         public string Description { get; set; }
         [MaxLength(50)]
+        [DisplayName("نویسنده")]
         public string Author { get; set; }
         public int Ganre { get; set; }
         public DateTime? BorrowDateTime { get; set; }
