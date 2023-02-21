e90a2884b2472278792a566165b0a6342e6fb52b
alinoruzi <alinoruzi.ir@gmail.com>
2023-02-21 20:10:10 +0330

    After presentation

--- LibraryManagement/Models/Book.cs
+++ LibraryManagement/Models/Book.cs
@@ -10,13 +10,13 @@ namespace LibraryManagement.Models
         public int Id { get; set; }
         [Required]
         [MaxLength(40)]
-        [DisplayName("عنوان")]
+        [Display(Name = "عنوان")]
         public string Title { get; set; }
         [MaxLength(350)]
-        [DisplayName("توضیحات")]
+        [Display(Name = "توضیحات")]
         public string Description { get; set; }
         [MaxLength(50)]
-        [DisplayName("نویسنده")]
+        [Display(Name ="نویسنده")]
         public string Author { get; set; }
         public int Ganre { get; set; }
         public DateTime? BorrowDateTime { get; set; }
